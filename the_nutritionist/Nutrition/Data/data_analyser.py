358a17c68aba542bf8028bc0daad9be5bea9c472
manasagar <manasagarwal12921@gmail.com>
2024-03-02 11:00:42 +0530

    commit

--- the_nutritionist/Nutrition/Data/data_analyser.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-x=pd.read_csv("/Users/manasagarwal/Documents/HackIIITP/the_nutritionist/Nutrition/Data/dataset1.csv")
-print(x.iloc[:,[5,6,7,10,12,13,15]])
-# y=pd.read_csv("/Users/manasagarwal/Documents/HackIIITP/the_nutritionist/Nutrition/Data/dataset.csv")
-# print(y.head())
-#the_nutritionist/Nutrition/Data/indian_rda_based_diet_recommendation_system.csv
-#the_nutritionist/Nutrition/Data/data_analyser.py
